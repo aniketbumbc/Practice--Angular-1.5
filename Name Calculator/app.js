f26c5b717d1982e8f09f0118a61d5dc24a5863c0
aniket umbc <aniket.umbc@gmail.com>
2018-06-09 17:38:40 +0530

    Test File

--- Name Calculator/app.js
+++ Name Calculator/app.js
@@ -2,13 +2,15 @@
     'use strict';
 
     angular.module('NameCalculator', [])
-        .controller('NameCalculatorController', function ($scope) {
+        .controller('NameCalculatorController', function ($scope,$filter) {
             $scope.name = " ";
             $scope.totalValue = 0;
             $scope.displayNo = function () {
                 var totalNameValue = calculateString($scope.name); //get total value
-                $scope.totalValue = totalNameValue;
+                $scope.totalValue = totalNameValue;             
+            
             };
+           
             function calculateString(string) {
                 var totalStringValue = 0;
                 for (var i = 0; i < string.length; i++) {
