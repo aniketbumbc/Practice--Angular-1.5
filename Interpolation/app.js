1173dd5b54cd0be51984d438c7765c6f66bd233a
aniket umbc <aniket.umbc@gmail.com>
2018-06-11 23:28:40 +0530

    Filter apply

--- Interpolation/app.js
+++ Interpolation/app.js
@@ -2,9 +2,10 @@
     'use strict';
     angular.module("Myapp", []).controller("Mycontroller", Myfunction);
 
-    Myfunction.$inject = ['$scope'];
-    function Myfunction($scope) {
+    Myfunction.$inject = ['$scope','$filter'];
+    function Myfunction($scope,$filter) {
         $scope.name = "Bunny Eats";
+        $scope.bunCost = 43;
         //$scope.needFood = '';
         // $scope.noNeedFood =  'NoFood';
         $scope.noFeedMe = function () {
@@ -15,6 +16,12 @@
             alert("Yeesss");
             $scope.needFood = 'NeedFood';
         };
+
+        $scope.Yahoomsg = function () {
+            var msg = "i love cookies";
+            var outmsg = $filter('uppercase')(msg);
+            return outmsg;
+          }
     }
  
 })();
