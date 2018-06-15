edd0ab16cb10990ee16921677d733e009882ed41
aniket umbc <aniket.umbc@gmail.com>
2018-06-15 06:15:38 +0530

    Digest and Apply, timeout

--- Digiest/app.js
+++ Digiest/app.js
@@ -2,14 +2,29 @@
     'use strict'; //
     angular.module('HelloApp', [])
         .controller('HelloController', HelloController);
-        
-        HelloController.$inject = ['$scope'];
-        
-        function HelloController($scope) {
 
-            $scope.counter = 0 ;
-            $scope.upCounter = function () {                
+    HelloController.$inject = ['$scope','$timeout'];
+
+    function HelloController($scope,$timeout) {
+        $scope.counter = 0;
+        $scope.upCounter = function () {
+            $timeout(function(){
                 $scope.counter++;
-            }
-        }
+                console.log("counter up");
+            },2000);
+            
+        }        // 
+        //     setTimeout(function () {
+        //         $scope.counter++;
+        //         console.log("counter up");
+        //         $scope.$digest();
+        //     }, 2000);
+        //
+        // setTimeout(function () { 
+        //     $scope.$apply(function(){
+        //         $scope.counter++;
+        //         console.log("counter up");
+        //     });
+        //  },2000); 
+    }
 })();
