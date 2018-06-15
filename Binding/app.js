a765a0680158d448b84de8f2ff9318e6756821c2
aniket umbc <aniket.umbc@gmail.com>
2018-06-15 06:29:17 +0530

    Binding

---
+++ Binding/app.js
@@ -0,0 +1,17 @@
+(function(){
+
+    angular.module("HelloApp",[]).
+    controller("HelloController",HelloController);
+
+    HelloController.$inject['$scope'];
+
+    function HelloController ($scope){
+
+        $scope.name = "Bunny";
+        $scope.userName = "";
+
+        $scope.setFullName = function (){
+            $scope.fullName =  $scope.name + " " + Yahoo;
+        }
+    }
+});
