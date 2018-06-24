e02396dc2c37b302ee4d0239e4137495061a85bf
aniket umbc <aniket.umbc@gmail.com>
2018-06-24 18:00:01 +0530

    HTTP service with scope and params

--- Http Service/app.js
+++ Http Service/app.js
@@ -3,36 +3,61 @@
 
     angular.module("HelloApp", [])
         .controller("MenuList", MenuList)
-        .service('MenuListService', MenuListService);
-        
+        .service('MenuListService', MenuListService)
+        .constant('ApiBasePath',"http://davids-restaurant.herokuapp.com");
+
 
 
     //Controller 1
-    MenuList.$inject = ['MenuListService'];
+    MenuList.$inject = ['MenuListService','$scope'];
 
-    function MenuList(MenuListService) {
+    function MenuList(MenuListService, $scope) {
         var menulist = this;
+        $scope.information;
         var promise = MenuListService.getMenu();
 
         promise.then(function (response) {
             menulist.items = response.data;
-            console.log( menulist.items);
+            console.log(menulist.items);
         }).catch(function (error) {
             console.log("Something wrong");
         });
+
+        menulist.logMenuItems = function (shortName) {
+            var promise = MenuListService.getMenuCategory(shortName);
+
+            promise.then(function (response) {
+                console.log(response.data);
+                $scope.information = response.data.category.name;
+            }).catch(function (error) {
+                console.log(error);
+            })
+        };
     }
 
-    MenuListService.$inject = ['$http'];
+    MenuListService.$inject = ['$http','ApiBasePath'];
 
-    function MenuListService($http){
+    function MenuListService($http,ApiBasePath) {
         var service = this;
 
-        service.getMenu = function(){
+        service.getMenu = function () {
             var response = $http({
-            method:"GET",
-            url:("http://davids-restaurant.herokuapp.com/categories.json")
+                method: "GET",
+                url: ApiBasePath + "/categories.json"
             });
             return response;
         }
-    }    
+    
+    service.getMenuCategory = function (shortName) {
+        var response = $http({
+            method: "GET",
+            url: ApiBasePath + "/menu_items.json",
+            params: {
+                category: shortName
+            }
+        });
+        return response;
+    };
+    }
+
 })();
