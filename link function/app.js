55183c9f88237247a470aee1955df6317b456273
aniket umbc <aniket.umbc@gmail.com>
2018-07-01 11:42:59 +0530

    Link function added

--- link function/app.js
+++ link function/app.js
@@ -26,7 +26,44 @@ function ShoppingListDirective() {
 
 
 function ShoppingListDirectiveLink(scope, element, attrs, controller) {
-  
+  console.log("Link scope is: ", scope);
+  console.log("Controller instance is: ", controller);
+  console.log("Element is: ", element);
+
+  scope.$watch('list.cookiesInList()', function (newValue, oldValue) {
+    console.log("Old value: ", oldValue);
+    console.log("New value: ", newValue);
+
+    if (newValue === true) {
+      displayCookieWarning();
+    }
+    else {
+      removeCookieWarning();
+    }
+
+  });
+
+  function displayCookieWarning() {
+    // Using Angluar jqLite
+    // var warningElem = element.find("div");
+    // console.log(warningElem);
+    // warningElem.css('display', 'block');
+
+    // If jQuery included before Angluar
+    var warningElem = element.find("div.error");
+    warningElem.slideDown(900);
+  }
+
+
+  function removeCookieWarning() {
+    // Using Angluar jqLite
+    // var warningElem = element.find("div");
+    // warningElem.css('display', 'none');
+
+    // If jQuery included before Angluar
+    var warningElem = element.find("div.error");
+    warningElem.slideUp(900);
+  }
 }
 
 
