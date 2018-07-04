4de362efd4fcec49dde8ca6a05829bd6aa7b58e4
aniket umbc <aniket.umbc@gmail.com>
2018-07-04 07:29:33 +0530

    Component Bug Free

--- component/app.js
+++ component/app.js
@@ -9,85 +9,28 @@ angular.module('ShoppingListComponentApp', [])
   controller: ShoppingListComponentController,
   bindings:{
     items:'<',
-    myTitle:'@tittle',
+    myTitle:'@title',
     onRemove:'&'
   }
 });
 
-
-// function ShoppingListDirective() {
-//   var ddo = {
-   
-//     scope: {
-//       items: '<',
-//       myTitle: '@title',
-//       onRemove: '&'
-//     },
-//     controller: ShoppingListDirectiveController,
-//     controllerAs: 'list',
-//     bindToController: true,
-//     link:ShoppingListDirectiveLink,
-//     transclude:true
-//   };
-
-//   return ddo;
-// }
-
-// Dom manupulation done inside in link function
-function ShoppingListDirectiveLink(scope, element, attrs, controller) {
-  console.log("Link scope is: ", scope);
-  console.log("Controller instance is: ", controller);
-  console.log("Element is: ", element);
-
-  scope.$watch('list.cookiesInList()', function (newValue, oldValue) {
-    console.log("Old value: ", oldValue);
-    console.log("New value: ", newValue);
-
-    if (newValue === true) {
-      displayCookieWarning();
-    }
-    else {
-      removeCookieWarning();
-    }
-
-  });
-
-  function displayCookieWarning() {
-    // Using Angluar jqLite
-    // var warningElem = element.find("div");
-    // console.log(warningElem);
-    // warningElem.css('display', 'block');
-
-    // If jQuery included before Angluar
-    var warningElem = element.find("div.error");
-    warningElem.slideDown(900);
-  }
-
-
-  function removeCookieWarning() {
-    // Using Angluar jqLite
-    // var warningElem = element.find("div");
-    // warningElem.css('display', 'none');
-
-    // If jQuery included before Angluar
-    var warningElem = element.find("div.error");
-    warningElem.slideUp(900);
-  }
-}
-
-
-function ShoppingListDirectiveController() {
-  var list = this;
-
-  list.cookiesInList = function () {
-    for (var i = 0; i < list.items.length; i++) {
-      var name = list.items[i].name;
-      if (name.toLowerCase().indexOf("cookie") !== -1) {
+function ShoppingListComponentController() {
+  var $ctrl = this;
+ 
+  $ctrl.cookiesInList = function () {
+    for (var i = 0; i < $ctrl.items.length; i++) {
+      var name = $ctrl.items[i].name;
+      if (name.toLowerCase().indexOf("mango") !== -1) {
         return true;
       }
     }
-
     return false;
+  };
+
+
+  $ctrl.Remove = function(myIndex){
+    //reference function
+    $ctrl.onRemove({index:myIndex});
   };
 }
 
@@ -101,20 +44,21 @@ function ShoppingListController(ShoppingListFactory) {
 
   viewList.items = shoppingList.getItems();
   var origTitle = "Shopping List #1";
- viewList.warn = "Ohh no Cokkiessss";
+ viewList.warn = "Ohh no Mangoooooss";
   viewList.title = origTitle + " (" + viewList.items.length + " items )";
 
   viewList.itemName = "";
   viewList.itemQuantity = "";
 
   viewList.addItem = function () {
-    shoppingList.addItem(viewList.itemName, viewList.itemQuantity);
+    shoppingList.addItem(viewList.itemName, viewList.itemQuantity);    
     viewList.title = origTitle + " (" + viewList.items.length + " items )";
+   
   };
 
   viewList.removeItem = function (itemIndex) {
     console.log("'this' is: ", this);
-    this.lastRemoved = "Last item removed was " + this.items[itemIndex].name;
+    this.lastRemoved = this.items[itemIndex].name + " Remove form List ";
     shoppingList.removeItem(itemIndex);
     this.title = origTitle + " (" + viewList.items.length + " items )";
   };
@@ -128,12 +72,12 @@ function ShoppingListService(maxItems) {
   // List of shopping items
   var items = [];
 
-  service.addItem = function (itemName, quantity) {
+  service.addItem = function (itemName, itemQuantity) {
     if ((maxItems === undefined) ||
         (maxItems !== undefined) && (items.length < maxItems)) {
       var item = {
         name: itemName,
-        quantity: quantity
+        quantity: itemQuantity
       };
       items.push(item);
     }
